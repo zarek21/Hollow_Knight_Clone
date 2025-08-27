dd2b769ef329c65993f8c5347ced4dd83bc20494
Zarek Saleme <jorge.saleme77@gmail.com>
2025-08-27 08:50:09 -0600

    :sparkles: add MovementPlayer script

---
+++ Assets/Scripts/MovementPlayer.cs
@@ -0,0 +1,20 @@
+// using - Librerías -  Funciones prestadas de otros scripts 
+using UnityEngine;
+
+public class MovementPlayer : MonoBehaviour
+{
+    // Variables
+    [Header("Configuraciones")]
+    public float speed = 0f;
+
+    private void Start()
+    {
+        print("HOLA, SOY START");
+    }
+
+    private void Update()
+    {
+        print("HOLA, SOY UPDATE");
+    }
+
+}  // end - class
