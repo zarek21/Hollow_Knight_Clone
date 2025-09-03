bb2354f10a4367616ba2dff670ada594b3e6acf0
Zarek Saleme <jorge.saleme77@gmail.com>
2025-09-03 08:02:02 -0600

    :sparkles: add scripting for player movement

--- Assets/Scripts/MovementPlayer.cs
+++ Assets/Scripts/MovementPlayer.cs
@@ -1,9 +1,16 @@
 // using - Librerías -  Funciones prestadas de otros scripts 
 using UnityEngine;
 
+/// <summary>
+///  Zarek 03 septiembre 2025
+/// </summary>
+
+
 public class MovementPlayer : MonoBehaviour
 {
     // Variables
+    [Header("Propiedades")]
+    public Transform transformPlayer;
     [Header("Configuraciones")]
     public float playerSpeed = 0.0f;
     private int playerHealth = 100;
