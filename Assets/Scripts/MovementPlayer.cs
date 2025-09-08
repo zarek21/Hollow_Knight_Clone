1f4fcb51946d3b3255c78ac0a2db147143910ca1
Zarek Saleme <jorge.saleme77@gmail.com>
2025-09-08 07:33:01 -0600

    :sparkles: add force

--- Assets/Scripts/MovementPlayer.cs
+++ Assets/Scripts/MovementPlayer.cs
@@ -8,30 +8,65 @@ using UnityEngine;
 
 public class MovementPlayer : MonoBehaviour
 {
-    // Variables
-    [Header("Propiedades Jugador")]
+    //Variables 
     public Transform transformPlayer;
 
-    [Header("Configuraciones Movimiento")]
-    public float playerSpeed = 0.0f;
+    public Rigidbody2D rigidBody2DPlayer;
 
-    private void Start()
-    {
-    }
+    /*
+public Transform transformPlayer;
+public Rigidbody2D rigidbodyPlayer;
+    */
 
-    private void Update()
+    // donde empieza el Frame 1. Frame 2 dejo de llamarse 
+    void Start()
     {
-        // Movimiento Izquierda
-        if (Input.GetKeyDown(KeyCode.A)) 
+        print("Start inicia aqui");
+        //Jala el rb del objeto
+        rigidBody2DPlayer = GetComponent<Rigidbody2D>();
+    }//end Start
+
+
+    // Desde Frame 2 hasta que termine el juego
+    //Loop que se llama toooodo el tiempo 
+    //Funciona mejor o peor dependiente de la PC, no tiene la
+    void Update()
+    {
+        //Movimiento Izq
+        if (Input.GetKeyDown(KeyCode.A))
         {
-            print("Vamos a la izquierda");
+            print("Vamos a la izq");
+
+            //Ignora las fisicas 
+            //transformPlayer.position += new Vector3(-1f,0f,0f);
+            rigidBody2DPlayer.AddForce(Vector2.left * 5.0f);
+
         }
 
-        // Movimiento Derecha
+        //Movimiento Der
         if (Input.GetKeyDown(KeyCode.D))
         {
-            print("Vamos a la derecha");
+            print("Vamos a la Der");
+            //transformPlayer.position +=  Vector3.right;
+            rigidBody2DPlayer.AddForce(Vector2.left * 5.0f);
+
         }
+        /*
+        print("Update inicia aqui");
+        if(Input.GetKeyDown (KeyCode.A))
+        {
+            //transformPlayer.position += new Vector3(1,0,0);
+            //transformPlayer.position +=  Vector3.right * 1f;
+            rigidbodyPlayer.AddForce(Vector2.right);
+        }
+        */
+
+    }//end Update
+
+
+    //Tasa fija de Frames
+    private void FixedUpdate()
+    {
 
     }
 
